2f4aadb1c07d7103b4501d3bcc4675ff4fa491b0
Sol-IoTiv <70333661+Sol-IoTiv@users.noreply.github.com>
2025-06-27 16:06:52 +0200

    module.php aktualisieren

--- PVWallboxManager/module.php
+++ PVWallboxManager/module.php
@@ -77,6 +77,8 @@ class PVWallboxManager extends IPSModule
         // Timer für regelmäßige Berechnung
         $this->RegisterTimer('PVUeberschuss_Berechnen', 0, 'IPS_RequestAction(' . $this->InstanceID . ', "UpdateCharging", 0);');
         $this->RegisterTimer('ZyklusLadevorgangCheck', 0, 'IPS_RequestAction(' . $this->InstanceID . ', "ZyklusLadevorgangCheck", 0);');
+        
+        $this->RegisterPropertyBoolean('ModulAktiv', true);
     }
     
     public function ApplyChanges()
