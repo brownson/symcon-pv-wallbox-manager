5041cd0cf3deae5108d86dba6c60f7c34a811dd9
Sol-IoTiv <70333661+Sol-IoTiv@users.noreply.github.com>
2025-07-27 20:41:56 +0200

    PV-Überschuss live

--- PVWallboxManager/module.php
+++ PVWallboxManager/module.php
@@ -103,7 +103,7 @@ class PVWallboxManager extends IPSModule
         $this->RegisterVariableFloat('PV_Ueberschuss','☀️ PV-Überschuss (W)',                               'PVWM.Watt', 10);
         IPS_SetIcon($this->GetIDForIdent('PV_Ueberschuss'), 'solar-panel');
 
-        $this->RegisterVariableFloat('PV_UeberschussLive', '☀️ PV-Überschuss (live)',                       'PVWM.Watt', 11);
+        $this->RegisterVariableFloat('PV_UeberschussLive', '☀️ PV-Überschuss (W) (live)',                       'PVWM.Watt', 11);
         IPS_SetIcon($this->GetIDForIdent('PV_UeberschussLive'), 'solar-panel');
 
         $this->RegisterVariableInteger('PV_Ueberschuss_A', '⚡ PV-Überschuss (A)',                             'PVWM.Ampere', 12);
